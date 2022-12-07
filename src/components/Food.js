3339d1362ff134f4009141deaae47f91f20fd3a4
nasirdiu <nasir.joypurhat@gmail.com>
2022-12-07 10:56:08 +0600

    food

--- src/components/Food.js
+++ src/components/Food.js
@@ -1,11 +1,81 @@
-import React from 'react';
-
+import React, { useState } from "react";
+import { data } from "../data/data";
 const Food = () => {
-    return (
+  //   console.log(data);
+  const [food, setFood] = useState(data);
+  return (
+    <div className="max-w-[1600px] w-full px-4 py-12">
+      <h1 className="text-orange-600 text-center font-bold text-4xl">
+        Top Rated Menu Items
+      </h1>
+      {/* filter row  */}
+      <div className="flex flex-col lg:flex-row justify-between">
+        {/* filter type  */}
         <div>
-            
+          <p className="font-bold text-gray-700">Filter Type</p>
+          <div className="flex justify-between flex-wrap">
+            <button className="m-1 border-orange-600 text-orange-600 hover:bg-orange-500 hover:text-white">
+              All
+            </button>
+            <button className="m-1 border-orange-600 text-orange-600 hover:bg-orange-500 hover:text-white">
+              burger
+            </button>
+            <button className="m-1 border-orange-600 text-orange-600 hover:bg-orange-500 hover:text-white">
+              pizza
+            </button>
+            <button className="m-1 border-orange-600 text-orange-600 hover:bg-orange-500 hover:text-white">
+              salad
+            </button>
+            <button className="m-1 border-orange-600 text-orange-600 hover:bg-orange-500 hover:text-white">
+              chicken
+            </button>
+          </div>
         </div>
-    );
+        {/* filter price  */}
+        <div>
+          <p className="font-bold text-gray-700">Filter Price</p>
+          <div className="flex justify-between w-full max-w-[390px]">
+            <button className="m-1 border-orange-600 text-orange-600 hover:bg-orange-500 hover:text-white">
+              $
+            </button>
+            <button className="m-1 border-orange-600 text-orange-600 hover:bg-orange-500 hover:text-white">
+              $$
+            </button>
+            <button className="m-1 border-orange-600 text-orange-600 hover:bg-orange-500 hover:text-white">
+              $$$
+            </button>
+            <button className="m-1 border-orange-600 text-orange-600 hover:bg-orange-500 hover:text-white">
+              $$$$
+            </button>
+          </div>
+          {/* display food  */}
+        </div>
+      </div>
+      {/* display food  */}
+      <div className="grid grid-cols-2 lg:grid-cols-4 gap-6 pt-4">
+        {food.map((item, index) => (
+          <div
+            key={index}
+            className="border shadow-lg hover:scale-105 duration-300 rounded-lg"
+          >
+            <img
+              src={item.image}
+              alt=""
+              className="w-full h-[200px] object-cover rounded-t-lg"
+            />
+            <div className="flex justify-between px-2 py-4">
+              <p className="font-bold">{item.name}</p>
+              <p>
+                <span className="bg-orange-500 text-white rounded-full p-1">
+                  {item.price}
+                </span>
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Food;
--- src/components/HeadlineCards.js
+++ src/components/HeadlineCards.js
@@ -5,9 +5,11 @@ const HeadlineCards = () => {
     <div className="max-w-[1640px] mx-auto p-4 grid md:grid-cols-3 gap-6 py-12">
       <div className="rounded-xl relative">
         <div className="absolute w-full h-full bg-black/50 rounded-xl text-white">
-          <p className='font-bold text-2xl px-2 pt-4'>New Restaurants</p>
+          <p className="font-bold text-2xl px-2 pt-4">New Restaurants</p>
           <p className="px-2">Added Daily</p>
-          <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Order Now</button>
+          <button className="border-white bg-white text-black mx-2 absolute bottom-4">
+            Order Now
+          </button>
         </div>
         <img
           className="max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl"
@@ -17,9 +19,11 @@ const HeadlineCards = () => {
       </div>
       <div className="rounded-xl relative">
         <div className="absolute w-full h-full bg-black/50 rounded-xl text-white">
-          <p className='font-bold text-2xl px-2 pt-4'>Sun's Out, BOGO's Out</p>
+          <p className="font-bold text-2xl px-2 pt-4">Sun's Out, BOGO's Out</p>
           <p className="px-2">Through 8/26</p>
-          <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Order Now</button>
+          <button className="border-white bg-white text-black mx-2 absolute bottom-4">
+            Order Now
+          </button>
         </div>
         <img
           className="max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl"
@@ -29,9 +33,13 @@ const HeadlineCards = () => {
       </div>
       <div className="rounded-xl relative">
         <div className="absolute w-full h-full bg-black/50 rounded-xl text-white">
-          <p className='font-bold text-2xl px-2 pt-4'>We Deliver Desserts Too</p>
+          <p className="font-bold text-2xl px-2 pt-4">
+            We Deliver Desserts Too
+          </p>
           <p className="px-2">Tasty Treats</p>
-          <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Order Now</button>
+          <button className="border-white bg-white text-black mx-2 absolute bottom-4">
+            Order Now
+          </button>
         </div>
         <img
           className="max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl"
